a242d5e09e6e145530825c09fc44781a93e70816
jv <jeff@jeffvier.com>
2020-12-09 21:35:32 -0800

    add a fast mode

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@
 # * Created by Jeff Vier on 7 Dec 2020
 # * https://github.com/boinger/maturitymodeler
 
-npm list -g requirejs || npm install -g requirejs
-mkdir dist/ || echo 'dist/ folder already exists...'
+if [ $1 != "fast" ]; then
+  npm list -g requirejs || npm install -g requirejs
+  mkdir dist/ || echo 'dist/ folder already exists...'
+fi
 cp -f js/require_2_3_6/require.min.js dist/
 cp -f favicon.png dist/
 node build/r_2_3_6/r.js -o build/build.js
